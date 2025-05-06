7bdb0dd5b156aa48632b31c77a11f27fda62a375
irfan <irfanabliz914@gmail.com>
2025-05-06 13:44:51 -0400

    The exit builtin

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,14 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         let ret = stdin.read_line(&mut input).unwrap();
-        println!("{}: command not found", input.trim());
+
+        let input = input.trim();
+
+        if input == "exit 0" {
+            std::process::exit(0);
+        }
+
+        println!("{}: command not found", input);
 
         if ret == 0 {
             // EOF reached, exit the loop
